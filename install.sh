3db11a06a3b9d7b8473b833238f9a8a203a60e15
David Bain <david@alteroo.com>
2013-01-01 14:28:11 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
+echo "adding customized files"
 cp plone-todo-tutorial-on-dotcloud/buildout.d/development.cfg buildout.d/
 cp plone-todo-tutorial-on-dotcloud/*.conf .
 mv setup.py setup.py.zz
 cp plone-todo-tutorial-on-dotcloud/getplone.sh .
 cp plone-todo-tutorial-on-dotcloud/dotcloud.yml .
+
+echo "removing the docs folder, it is bulky and not needed for dotcloud"
+rm -rf docs/*
 
