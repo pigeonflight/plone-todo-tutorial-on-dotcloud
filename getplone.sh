ada95cac2ee473aade5412e2b1dd5d3b7dc45710
David Bain <david@alteroo.com>
2013-01-01 15:34:07 -0500

    Update getplone.sh

--- getplone.sh
+++ getplone.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 current=/home/dotcloud/current
-eggbundle=https://plone-egg-packages.googlecode.com/files/eggs-plone-4.2.3-py2.7-jan012012-1.tgz
+eggbundle=https://plone-egg-packages.googlecode.com/files/eggs-plone-4.2.3-py2.7-jan012012-2.tgz
 cd $current
 python bootstrap.py
 # dotcloud will try to get the setup.py dependencies 
@@ -8,7 +8,7 @@ python bootstrap.py
 # here we put it back
 mv setup.py.zz setup.py
 rm -rf eggs
-wget https://plone-egg-packages.googlecode.com/files/eggs-plone-4.2.3-py2.7-jan012012-1.tgz -O eggs.tar.gz
+wget $eggbundle -O eggs.tar.gz
 #curl -o eggs.tar.gz $eggbundle
 gunzip eggs.tar.gz
 tar xf eggs.tar
